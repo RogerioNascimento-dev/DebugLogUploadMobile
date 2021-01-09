04ad2ace6e4096e7aad615610fb2d28d6c59aac0
Rogério Nascimento <rogerio.nascimento@soterotech.com.br>
2021-01-09 15:55:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+
+namespace DebugLogUploadMobile
+{
+
+    class Program
+    {
+
+        static HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection();
+            try
+            {
+                //Inicia a conexão com o banco de dados
+                con.ConnectionString = @"Data Source=.;Initial Catalog=CadastroNetAssetDb_Demo;Integrated Security=True";
+                if (con.State == System.Data.ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                //Obtendo parâmetro de filtro
+                String serviceOrderTaskId;
+                Console.Write("Informe o parâmetro serviceOrderTaskId: ");
+                serviceOrderTaskId = Console.ReadLine();
+
+                //Query String
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = @"SELECT [Id],[ServiceOrderTaskId],[UserId],[Date],[Json],[IsSuccess]" +
+                                " FROM dbo.LogUploadMobile log  WHERE log.ServiceOrderTaskId =" +serviceOrderTaskId+
+                                " ORDER BY log.id ASC";
+                
+                cmd.Connection = con;                
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                var filepath = @"C:\Users\roger\Documents\fotosDebug";
+                int i = 1;
+                while (reader.Read())
+                {
+                    var payload = Regex.Unescape(reader.GetString(4)).TrimEnd('\"').TrimStart('\"');
+                    RequestServiceOrderTask obj = JsonConvert.DeserializeObject<RequestServiceOrderTask>(payload);
+                    if (!string.IsNullOrEmpty(obj.ItemData))
+                    {
+
+                        var path = filepath + "\\" + obj.Id + "\\Fotos\\";
+                        var bytes = Convert.FromBase64String(obj.ItemData);
+
+                        if (!Directory.Exists(path))
+                            Directory.CreateDirectory(path);
+                        var fullFilepath = path + obj.Id + "_" + i + ".jpg";                        
+                        using (var imageFile = new FileStream(fullFilepath, FileMode.Create))
+                        {
+                            imageFile.Write(bytes, 0, bytes.Length);
+                            imageFile.Flush();
+                        }
+                        Console.WriteLine("Imagem salva em: "+ fullFilepath);
+                    }
+                    else {
+                        Console.WriteLine("Enviando dados para o endpoint: /api/mobile/synchronization");
+                        PostApi(payload);                        
+                    }                    
+                    i++;
+                }
+
+                reader.Close();
+                reader.Dispose();
+                con.Close();               
+
+            }
+            catch (SqlException)
+            {
+                Console.WriteLine("Algum erro aconteceu.");
+            }
+        }
+        
+
+        public async static void PostApi(string payload)
+        {           
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:60231/api/mobile/synchronization");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                
+                streamWriter.Write(payload);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            try
+            {
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }           
+
+        }            
+        
+    }
+}
---
+++ RequestServiceOrderTask.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace DebugLogUploadMobile
+{
+    public class RequestServiceOrderTask
+    {
+        public string UserId { get; set; }
+        public int Id { get; set; }
+        public int ServiceOrderId { get; set; }
+        public double Xp { get; set; }
+        public double Yp { get; set; }
+        public string Tasks { get; set; }
+        public string TasksStatus { get; set; }
+        public string TasksFeedback { get; set; }
+        public int Target { get; set; }
+        public string TargetIdAliasLabel { get; set; }
+        public string TargetIdAlias { get; set; }
+        public int Status { get; set; }
+        public string PhotosWork { get; set; }
+        public bool Exclude { get; set; }
+        public string Extra { get; set; }
+        public string PhotoSlot { get; set; }
+        public int CommitedStatus { get; set; }
+        public string ItemData { get; set; }
+        
+
+    }
+}
